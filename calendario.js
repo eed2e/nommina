2704c5c30469706f9a3409c35339c38b542fdec2
Danilgopez1702 <80540485+Danilgopez1702@users.noreply.github.com>
2021-03-12 14:19:57 -0600

    prueba

---
+++ calendario.js
@@ -0,0 +1,32 @@
+$.datepicker.regional['es'] = {
+  closeText: 'Cerrar',
+  prevText: '<Ant',
+  nextText: 'Sig>',
+  currentText: 'Hoy',
+  monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+  dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+  dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+  weekHeader: 'Sm',
+  dateFormat: 'dd/mm/yy',
+  firstDay: 1,
+  isRTL: false,
+  showMonthAfterYear: false,
+  yearSuffix: ''
+};
+
+$.datepicker.setDefaults($.datepicker.regional['es']);
+
+// ----------------- CREACIÓN DE DATEPICKER DE PRUEBA----------------------
+$(function() {
+    $("#nuestrodatepicker").datepicker();
+}
+
+
+// ----------------- DEFINICIÓN DE FORMATO ----------------------
+// Recurso original:
+// http://www.backtheweb.com/jquery/como-dar-formato-a-la-fecha-del-datepicker-de-jquery/
+$(function() {
+    $('#nuestrodatepicker').datepicker('option', {dateFormat: 'dd/mm/yy'});
+});
