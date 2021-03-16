3e86e61f8b08244e882cf1810161625799556785
eed2e <eed.esteban@gmail.com>
2021-03-16 12:37:40 -0600

    LOGIN

--- login.php
+++ login.php
@@ -1,18 +1,19 @@
 <?php
-  session_start();
- 
+
+ session_start();
+
   // Obtengo los datos cargados en el formulario de login.
   $user = $_POST['user'];
   $pass = $_POST['pass'];
    
   // Esto se puede remplazar por un usuario real guardado en la base de datos.
-  if($user == 'Carlos' && $pass == 'Carlos2021'){
+  if($user == 'carlos' && $pass == '1234'){
     // Guardo en la sesión el email del usuario.
     $_SESSION['user'] = $user;
      
     // Redirecciono al usuario a la página principal del sitio.
     
-    header("Location: inicio.php"); 
+    header("Location: inicio.html"); 
   }else{
     echo 'El Usuario o contraseña son incorrecto, <a href="index.html">vuelva a intenarlo</a>.<br/>';
   }
