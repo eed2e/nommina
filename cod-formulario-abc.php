41c71b36f7ad35c29bd88ecb2914873517e1e187
eed2e <eed.esteban@gmail.com>
2021-03-19 17:01:01 -0600

    abc con tabla mostrando solo falta completar bd

--- cod-formulario-abc.php
+++ cod-formulario-abc.php
@@ -12,33 +12,91 @@ spl_autoload_register(function( $NombreClase ) {
     require_once $NombreClase . '.php';
 });
 
-class Empleados {
+class Productos {
 
-    public $Id_Empleado = 0;
-    public $Nombre = "";
-    public $Apellido_Paterno = "";
-    public $Apellido_Materno = 0;
-    public $Fecha_Ingreso = 0;
+    public $id_empleado = 0;
+    public $codigo_empleado = "";
+    public $nombre = "";
+    public $sMensual = 0;
+    public $sDiario = 0;
+
+    function Eliminar($idEmpleado) {
+        $mysql = new Connection();
+        $cnn = $mysql->getConnection();
+        $retorno = $this->ArrayMessage("0", "No se ha realizado ninguna acción.");
+        $query = $cnn->prepare("call proc_ProductoEliminar (?)");
+        $query->bind_param("i", $idEmpleado);
+        $query->execute();
+        $query->store_result();
+        if (mysqli_stmt_error($query) != "") {
+            $retorno = $this->ArrayMessage("0", mysqli_stmt_error($query));
+        }
+        //Verificar si se obtubieron resultados
+        if ($query->num_rows != 0) {
+            $query->bind_result($this->id_empleado);
+            if ($query->fetch()) {
+                if (is_null($this->id_empleado)) {
+                    $retorno = $this->ArrayMessage("0", "No se ha realizado ninguna acción. El error se desconoce.");
+                } else {
+                    $retorno = $this->ArrayMessage("1", "El producto ha sido eliminado.");
+                }
+            }
+        }
+        $query->close();
+        $cnn->close();
+        return $retorno;
+    }
+
+
+    function Grabar() {
+        $mysql = new Connection();
+        $cnn = $mysql->getConnection();
+        $retorno = $this->ArrayMessage("0", "No se ha realizado ninguna acción.");
+        $query = $cnn->prepare("call proc_ProductoGrabar (?,?,?,?,?)");
+        $query->bind_param("issdd", $this->id_empleado, $this->codigo_empleado, $this->nombre, $this->sMensual, $this->sDiario);
+        $query->execute();
+        $query->store_result();
+        if (mysqli_stmt_error($query) != "") {
+            $retorno = $this->ArrayMessage("0", mysqli_stmt_error($query));
+        }
+        //Verificar si se obtubieron resultados
+        if ($query->num_rows != 0) {
+            $query->bind_result($this->id_empleado);
+            if ($query->fetch()) {
+                if (is_null($this->id_empleado)) {
+                    $retorno = $this->ArrayMessage("0", "No se ha realizado ninguna acción. El error se desconoce.");
+                } else {
+                    $retorno = $this->ArrayMessage("1", "El producto ha sido grabado correctamente.");
+                }
+            }
+        }
+        $query->close();
+        $cnn->close();
+        return $retorno;
+    }
+
+   
+
 
     function Buscar($textoBuscar) {
         $mysql = new Connection();
         $cnn = $mysql->getConnection();
         $retorno = array();
-        $query = $cnn->prepare("call proc_EmpleadoBuscar (?)");
+        $query = $cnn->prepare("call proc_ProductoBuscar (?)");
         $query->bind_param("s", $textoBuscar);
         $query->execute();
-        $empleado = new Empleados(); //Variable
+        $producto = new Productos(); //Variable
         $query->bind_result(
-                $Id_Empleado, $Nombre, $Apellido_Paterno, $Apellido_Materno, $Fecha_Ingreso
+                $id_empleado, $codigo_empleado, $nombre, $sMensual, $sDiario
         );
         while ($query->fetch()) {
-            $empleado = new Empleados();
-            $empleado->Id_Empleado = $Id_Empleado;
-            $empleado->Nombre = $Nombre;
-            $empleado->Apellido_Paterno = $Apellido_Paterno;
-            $empleado->Apellido_Materno = $Apellido_Materno;
-            $empleado->Fecha_Ingreso = $Fecha_Ingreso;
-            array_push($retorno, $empleado);
+            $producto = new Productos();
+            $producto->id_empleado = $id_empleado;
+            $producto->codigo_empleado = $codigo_empleado;
+            $producto->nombre = $nombre;
+            $producto->sMensual = $sMensual;
+            $producto->sDiario = $sDiario;
+            array_push($retorno, $producto);
         }
         $query->close();
         $cnn->close();
@@ -58,9 +116,25 @@ if (isset($_GET["functionToCall"]) && !empty($_GET["functionToCall"])) {
     $functionToCall = $_GET["functionToCall"];
     $json_data = json_decode(file_get_contents('php://input'));
     switch ($functionToCall) {
-        case "buscar_empleado":
-            $empleado = new Empleados();
-            echo json_encode($empleado->Buscar(utf8_decode($json_data->textoBuscar)));
+        case "buscar_producto":
+            $producto = new Productos();
+            echo json_encode($producto->Buscar(utf8_decode($json_data->textoBuscar)));
+            break;
+        
+        case "grabar_producto":
+            $producto = new Productos();
+            $producto->id_empleado = $json_data->id_empleado;
+            $producto->codigo_empleado = $json_data->codigo_empleado;
+            $producto->nombre = $json_data->nombre;
+            $producto->sMensual = $json_data->sMensual;
+            $producto->sDiario = $json_data->sDiario;
+            echo json_encode($producto->Grabar());
+            break;
+        
+        case "eliminar_producto":
+            $producto = new Productos();
+            echo json_encode($producto->Eliminar($json_data->id_producto));
             break;
     }
-}
+} 
+ 
--- formulario-abc.php
+++ formulario-abc.php
@@ -3,11 +3,6 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set("display_errors", 1);
 ?>
 <!DOCTYPE html>
-<?php
-error_reporting(E_ALL & ~E_DEPRECATED);
-ini_set("display_errors", 1);
-?>
-<!DOCTYPE html>
 <!--
  * Description of Productos
  *
@@ -20,24 +15,119 @@ ini_set("display_errors", 1);
         <!--Referencias CSS-->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body id="ng-empleado-lista" ng-app="appCatalogos" ng-controller="cEmpleados">
+    <body id="ng-producto-lista" ng-app="appCatalogos" ng-controller="cProductos">
         <!--PONER AQUÍ EL CÓDIGO HTML-->
+        <!-- Modal Eliminar-->
+<div id="modalProductoEliminar" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h4 class="modal-title" style="color:red;">¿Realmente desea eliminar al Empleado?</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label>Código del Empleado:</label>
+                        <div class="form-group input-group">
+
+                            <input type="text" ng-model="producto.codigo_empleado" readonly="true" class="form-control" />-->
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <label>Nombre del Empleado:</label>
+                            <input type="text" ng-model="producto.nombre" readonly="true" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" ng-click="Eliminar()" class="btn btn-danger" ><i class="glyphicon glyphicon-trash"></i> Eliminar</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal"><i class="glyphicon glyphicon-remove"></i> Cancelar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Eliminar-->
+        <!-- Modal Producto -->
+<div id="modalProducto" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h4 class="modal-title">Empleado</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label>Código del Empleado:</label>
+                        <div class="form-group input-group">
+
+                            <input type="text" id="txtCodigoBarras" ng-model="producto.codigo_empleado" class="form-control" />
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <label>Nombre del Empleado:</label>
+                            <input type="text" id="txtNombreProducto" ng-model="producto.nombre" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label>Salario Mensual:</label>
+                        <div class="form-group input-group">
+                            <input type="text" id="txtStock" ng-model="producto.sMensual" class="form-control" />
+                            <span class="input-group-addon">0.00</span>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <label>Salario Diario:</label>
+                        <div class="form-group input-group">
+
+                            <input type="text" id="txtStock" ng-model="producto.sDiario" class="form-control" />
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-usd"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" ng-click="Grabar()" class="btn btn-primary" ><i class="glyphicon glyphicon-floppy-disk"></i> Grabar</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal"><i class="glyphicon glyphicon-remove"></i> Cancelar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Modal Producto -->
         <div class="container-fluid">
-            <h1>Formulario A-B-C</h1>
+            <h1 class = "text-center"> DigitalNet</h1>
             <hr />
-            <a href="#" class="btn btn-primary" ><i class="glyphicon glyphicon-plus"></i> Agregar nuevo producto</a>
+            <a href="#" class="btn btn-primary" ng-click="AbrirNuevo();"><i class="glyphicon glyphicon-plus"></i> Agregar Empleado</a>
             <br /><br />
             <div class ="row">
                 <div class="col-sm-6">
                     <div class="form-group input-group">
-                    <input type="text" class="form-control" ng-keyup="$event.keyCode == 13 ? BuscarEmpleado() : null" id="txtTextoBuscar" placeholder="Id ó Nombre del Empleado" />
+                    <input type="text" class="form-control" ng-keyup="$event.keyCode == 13 ? BuscarProducto() : null" id="txtTextoBuscar" placeholder="Código de Empleado o nombre del Empleado" />
                         <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
                     </div>
                 </div>
             </div>
 
             <div class ="row">
-            <table class="table table-bordered table-striped table-hover">
+                <div class="col-sm-12">
+                <table class="table table-bordered table-striped table-hover">
     <thead>
         <tr>
             <th colspan="6" class="text-center">Listado de Empleados</th>
@@ -45,27 +135,26 @@ ini_set("display_errors", 1);
         <tr>
             <th class="text-center"><i class="glyphicon glyphicon-pencil"></i></th>
             <th class="text-center"><i class="glyphicon glyphicon-trash"></i></th>
-            <th>Id Empleado</th>
+            <th>Código del Empleado</th>
             <th>Nombre</th>
-            <th class="text-right">Apellido Paterno</th>
-            <th class="text-right">Apellido Materno</th>
+            <th class="text-right">Suldo Mensual</th>
+            <th class="text-right">Sueldo Diario</th>
         </tr>
     </thead>
     <tbody>
-        <tr ng-repeat="empleado in listaEmpleados">
-            <td class="text-center"><a href="#" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-pencil"></i></a></td>
-            <td class="text-center"><a href="#" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i></a></td>
-            <td>{{empleado.Id_Empleado}}</td>
-            <td>{{empleado.Nombre}}</td>
-            <td class="text-right">{{empleado.Apellido_Paterno}}</td>
-            <td class="text-right">{{empleado.Apellido_Materno}}</td>
+        <tr ng-repeat="producto in listaProductos">
+            <td class="text-center"><a href="#" ng-click="AbrirEditar(this.producto);" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-pencil"></i></a></td>
+            <td class="text-center"><a href="#" ng-click="AbrirEliminar(this.producto);" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i></a></td>
+            <td>{{producto.codigo_empleado}}</td>
+            <td>{{producto.nombre}}</td>
+            <td class="text-right">{{producto.sMensual|number:2}}</td>
+            <td class="text-right">{{producto.sDiario| number:2}}</td>
         </tr>
     </tbody>
 </table>
-            <div class="row text-center">
-                
+                </div>
             </div>
-        </div>
+         
         <!--Referencias Javascript-->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
@@ -74,26 +163,84 @@ ini_set("display_errors", 1);
         <script type="text/javascript">
             //Código Javascript
             var myApp = angular.module('appCatalogos', []);
-            
-            myApp.controller('cEmpleados', function ($scope, $http) {
-    var myData = {textoBuscar: ''};
-    $scope.empleado = {Id_Empleado: 0, Nombre: "", Apellido_Paterno: "", Apellido_Materno: "", Fecha_Ingreso: ""};
-    $http({
-        method: "POST",
-        url: 'cod-formulario-abc.php?functionToCall=buscar_empleado',
-        data: myData}).then(function (response) {
-        $scope.listaEmpleados = response.data;
-    });
-    $scope.BuscarEmpleado = function () {
-        var myData = {textoBuscar: String($("#txtTextoBuscar").val())};
-        $http({
-            method: "POST",
-            url: 'cod-formulario-abc.php?functionToCall=buscar_empleado',
-            data: myData}).then(function (response) {
-            $scope.listaEmpleados = response.data;
-        });
-    };
-});
+            myApp.controller('cProductos', function ($scope, $http) {
+                $scope.AbrirEliminar = function (item) {
+                    $scope.producto = item;
+                    $("#modalProductoEliminar").modal();
+                };
+                $scope.Eliminar = function () {
+                    $http({
+                        method: "POST",
+                        url: 'cod-formulario-abc.php?functionToCall=eliminar_producto',
+                        data: $scope.producto}).then(function (response) {
+                            if (response.data.status === "1") {
+                                alert(response.data.message);
+                                $scope.BuscarProducto();
+                                $("#modalProductoEliminar").modal("hide");
+                            } else {
+                                alert(response.data.message);
+                            }
+                        });
+                };
+                $scope.AbrirEditar = function (item) {
+                    $scope.AbrirEliminar = function (item) {
+                    $scope.producto = item;
+                    $("#modalProductoEliminar").modal();
+                    };
+                    $scope.Eliminar = function () {
+                    $http({
+                        method: "POST",
+                        url: 'cod-formulario-abc.php?functionToCall=eliminar_producto',
+                        data: $scope.producto}).then(function (response) {
+                            if (response.data.status === "1") {
+                                alert(response.data.message);
+                                $scope.BuscarProducto();
+                                $("#modalProductoEliminar").modal("hide");
+                            } else {
+                                alert(response.data.message);
+                            }
+                        });
+                    };
+                    $scope.producto = item;
+                    $("#modalProducto").modal();
+                };
+                $scope.AbrirNuevo = function () {
+                    $scope.producto = {id_producto: 0, codigo_barras: "", nombre_producto: "", stock: 0, precio_venta: 0};
+                    $("#modalProducto").modal();
+                };
+                $scope.Grabar = function () {
+                    $http({
+                        method: "POST",
+                        url: 'cod-formulario-abc.php?functionToCall=grabar_producto',
+                        data: $scope.producto}).then(function (response) {
+                        if (response.data.status === "1") {
+                            alert(response.data.message);
+                            $scope.BuscarProducto();
+                            $("#modalProducto").modal("hide");
+                        } else {
+                            alert(response.data.message);
+                        }
+                    });
+                };
+                var myData = {textoBuscar: ''};
+                $scope.producto = {id_producto: 0, codigo_barras: "", nombre_producto: "", stock: 0, precio_venta: 0};
+                $http({
+                    method: "POST",
+                    url: 'cod-formulario-abc.php?functionToCall=buscar_producto',
+                    data: myData}).then(function (response) {
+                    $scope.listaProductos = response.data;
+                });
+                $scope.BuscarProducto = function () {
+                    var myData = {textoBuscar: String($("#txtTextoBuscar").val())};
+                    $http({
+                        method: "POST",
+                        url: 'cod-formulario-abc.php?functionToCall=buscar_producto',
+                        data: myData}).then(function (response) {
+                        $scope.listaProductos = response.data;
+                    });
+                };
+            });
+
         </script>
     </body>
 </html>
